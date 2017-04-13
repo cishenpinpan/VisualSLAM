92af7e61b16b953e0b86158573cb0aaf409ac8df
hongyi <hongyi_fan@brown.edu>
2017-04-13 15:27:08 -0400

    Add new functions into Canvas class

--- include/Canvas.h
+++ include/Canvas.h
@@ -14,6 +14,7 @@
 #include "opencv2/core.hpp"
 #include "opencv2/features2d.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
 using namespace std;
 using namespace cv;
@@ -22,6 +23,16 @@ class Canvas
 {
 public:
     void drawFeatureMatches(Mat img1, Mat img2, vector<Point2f> points1, vector<Point2f> points2);
+	void drawSingleImage(Mat img1);
+
+	void drawKeyPoints(Mat img1, vector<Point2f> Keypoints1, string windowName);
+	void drawKeyPoints(Mat img1, vector<KeyPoint> Keypoints1, string windowName);
+
+
+	void drawTrackingPathEveryOtherK(Mat img, vector<KeyPoint> keyPoints1, vector<KeyPoint> keyPoints2, int numOfDrawing);
+	void drawLine(Mat img, Point2f start, Point2f end);
+	void drawTrackingPath(Mat img, vector<KeyPoint> keyPoints1, vector<KeyPoint> keyPoints2);
+	void drawTrackingPath(Mat img, vector<Point2f> keyPoints1, vector<Point2f> keyPoints2);
 };
 
 #endif /* Canvas_h */
--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -26,3 +26,106 @@ void Canvas::drawFeatureMatches(Mat img1, Mat img2, vector<Point2f> points1,vect
     waitKey(0);
 }
 
+
+void Canvas::drawSingleImage(Mat img1)
+{
+	namedWindow("Single image",WINDOW_AUTOSIZE);
+	imshow("Single image", img1);
+	waitKey(0);
+}
+
+void Canvas::drawKeyPoints(Mat img1, vector<Point2f> points1, string windowName)
+{
+	Mat canvas;
+	vector<KeyPoint> Keypoints1;
+	for (int i = 0; i < points1.size(); i++)
+	{
+		Keypoints1.push_back(KeyPoint(points1[i],1.f));
+	}
+	drawKeypoints(img1,Keypoints1,canvas);
+
+	namedWindow(windowName, WINDOW_AUTOSIZE);
+	imshow(windowName, canvas);
+	waitKey(0);
+
+}
+
+void Canvas::drawKeyPoints(Mat img1, vector<KeyPoint> points1, string windowName)
+{
+	Mat canvas;
+	drawKeypoints(img1, points1, canvas);
+
+	namedWindow(windowName, WINDOW_AUTOSIZE);
+	imshow(windowName, canvas);
+	waitKey(0);
+}
+
+
+
+void Canvas::drawTrackingPathEveryOtherK(Mat img, vector<KeyPoint> keyPoints1, vector<KeyPoint> keyPoints2, int numOfDrawing)
+{
+	vector<KeyPoint> tempKeyPoints1, tempKeyPoints2;
+	for (int i = 0; i < keyPoints1.size(); i += numOfDrawing)
+	{
+		tempKeyPoints1.push_back(keyPoints1[i]);
+		tempKeyPoints2.push_back(keyPoints2[i]);
+	}
+
+	drawTrackingPath(img, tempKeyPoints1, tempKeyPoints2);
+}
+
+
+
+void Canvas::drawLine( Mat img, Point2f start, Point2f end )
+{
+	int thickness = 0.5;
+	int lineType = LINE_8;
+	line( img,start,end,Scalar( 255, 0, 0 ),thickness,lineType );
+}
+
+void Canvas::drawTrackingPath(Mat img, vector<Point2f> keyPoints1, vector<Point2f> keyPoints2)
+{
+	for (int i = 0; i < keyPoints1.size(); i++)
+	{
+		drawLine(img, keyPoints1[i], keyPoints2[i]);
+	}
+
+	vector<KeyPoint> TempKeypoints1;
+	for (int i = 0; i < keyPoints1.size(); i++)
+	{
+		TempKeypoints1.push_back(KeyPoint(keyPoints1[i],1.f));
+	}
+	drawKeypoints(img,TempKeypoints1,img);
+	namedWindow("Tracking Path", WINDOW_AUTOSIZE);
+	imshow("Tracking Path", img);
+	waitKey(0);
+}
+
+void Canvas::drawTrackingPath(Mat img, vector<KeyPoint> keyPoints1, vector<KeyPoint> keyPoints2)
+{
+	vector<Point2f> tempPoints1, tempPoints2;
+	KeyPoint::convert(keyPoints1, tempPoints1);
+	KeyPoint::convert(keyPoints2, tempPoints2);
+	for (int i = 0; i < keyPoints1.size(); i++)
+	{
+
+		drawLine(img, tempPoints1[i], tempPoints2[i]);
+	}
+	drawKeypoints(img, keyPoints1, img);
+	namedWindow("Tracking Path", WINDOW_AUTOSIZE);
+	imshow("Tracking Path", img);
+	waitKey(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
