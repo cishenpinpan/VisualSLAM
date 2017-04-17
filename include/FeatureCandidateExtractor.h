5ed7c77841798f80d3220505a791658bb7a55bff
hongyi <hongyi_fan@brown.edu>
2017-04-17 15:56:06 -0400

    New Class: FeatureCandidateExtractor

---
+++ include/FeatureCandidateExtractor.h
@@ -0,0 +1,39 @@
+//
+//  FeatureCandidateDetector.hpp
+//  VisualSLAM
+//
+//  Created by Rong Yuan on 4/14/17.
+//  Copyright © 2017 Hongyi Fan. All rights reserved.
+//
+
+#ifndef FeatureCandidateExtractor_h
+#define FeatureCandidateExtractor_h
+
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "Feature.h"
+#include "Factory.h"
+#include "SystemParameters.h"
+#include <iostream>
+#include <stdio.h>
+#include <set>
+#include <map>
+
+using namespace std;
+using namespace cv;
+using namespace cv::xfeatures2d;
+
+class FeatureCandidateExtractor {
+public:
+	FeatureCandidateExtractor(){_featureName = FEATURE_NAME; _descriptorName = DESCRIPTOR_NAME;};
+    vector<KeyPoint> extractFeatures(Mat img);
+	Mat computeDescriptors(Mat img, vector<KeyPoint> keyPoints1);
+    //vector<KeyPoint> reextractFeatures(Mat img_1, FeatureSet& featureSet);
+private:
+	string _featureName;
+	string _descriptorName;
+
+};
+#endif /* FeatureCandidateExtractor_h */
